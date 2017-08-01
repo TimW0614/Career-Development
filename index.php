a0e2957bfa6b41eb8531e70fa16b5132929a8379
Tim Williams <tikytimbo5@yahoo.com>
2017-07-31 19:38:09 -0600

    Added pro dev document

---
+++ index.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<html>
+	<head>
+		<title>Career Developement</title>
+	</head><h2>Career Developement</h2>
+	<body>
+		<p><strong>Career Buddy:</strong>Ryan Henson</p>
+		<p><strong>3 quantitative accomplishments:</strong></p>
+		<ul>
+			<li>As a Table games dealer I must posses the ability to think quickly and analytically. Must accurately calculate
+			winning or losing hands in a matter of seconds.</li>
+			<li>As a Table Games Supervisor, I am responsible for analyzing customer play for cheating scams and suspicious
+			variations in play.</li>
+			<li>Held several positions that involve computing money (server, bartender etc..)</li>
+		</ul>
+
+		<p><strong>3 qualitative accomplishments</strong></p>
+		<ul>
+			<li>I have held Training and Development positions at multiple jobs</li>
+			<li>I work well in stressful and fast paced environments</li>
+			<li>Successfully completed Real Estate school</li>
+		</ul>
+
+			<p><strong>Golden Circles:</strong></p>
+			<p><strong>why:</strong>To have a successful career</p>
+			<p><strong>How:</strong>By attending a 10 week Bootcamp focusing on computer coding</p>
+			<p><strong>What:</strong>Become a proficient Web Developer</p>
+
+		<p><strong>Elevator Pitch:</strong>I am attending a very intense Full Stack Web Development Bootcamp, I am looking
+		for any opportunity to get my feet wet and prove my skills. My passion is coming up with cretive ways to express
+		a message and there is no better way than in web development. If you hear of any opportunities please send them my
+		way!</p>
+
+		<p><strong>What I found after searching myself:</strong>I found links to my social media as well as some pictures
+		that I have been linked in at various events.</p>
+		<p><strong>What I found after searching my buddy:</strong>When I searched for Ryan I found a link to his LinkeIn
+		profile. I looked at his profile and learned about his education and background.</p>
+
+
+	</body>
+</html>
